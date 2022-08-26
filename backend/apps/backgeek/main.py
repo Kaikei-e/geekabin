05ba0151763770e462132ded8fca57caa79f21e7
Kaikei-e <e_kaikei@protonmail.com>
2022-08-27 00:25:10 +0900

    Fix: error

--- backend/apps/backgeek/main.py
+++ backend/apps/backgeek/main.py
@@ -1,9 +1,11 @@
+from router.router import init_router
+
+
 def main():
     print("Hello World")
 
+    init_router()
 
 
 if __name__ == "__main__":
     main()
-
-
--- backend/apps/backgeek/router/router.py
+++ backend/apps/backgeek/router/router.py
@@ -5,7 +5,6 @@ from fastapi import APIRouter
 def init_router() -> FastAPI:
     app = FastAPI()
     router = APIRouter()
-    router.add_api_route("/", include_in_schema=False)
 
     app.include_router(router, prefix="/api")
 
