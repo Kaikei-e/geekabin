31ff2bd14fc50e8c22d00edea269e75cb358a48c
Kaikei-e <e_kaikei@protonmail.com>
2022-08-27 23:40:03 +0900

    Update: shakyoued JWT oauth

--- backend/apps/backgeek/authenticator/auth.py
+++ backend/apps/backgeek/authenticator/auth.py
@@ -1,10 +1,26 @@
+from datetime import datetime, timedelta
 from typing import Union
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import os
 from pydantic import BaseModel
 from passlib.context import CryptContext
+from jose import JWTError, jwt
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.environ['SECRET_KEY']
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+        "uuid": "fda57a70-cef8-4b2c-8fce-89a98e2f2985",
+    }
+}
 
 
 class Token(BaseModel):
@@ -34,10 +50,75 @@ print(os.environ['SECRET_KEY'])
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def oath_keeper(plain_password, hashed_password):
+def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
 
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+
+        return UserInDB(**user_dict)
+
+
+def oath_keeper(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_pass):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expure = datetime.utcnow() + expires_delta
+    else:
+        expure = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expure})
+
+    encoded_jwt = jwt.encode(to_encode, os.environ['SECRET_KEY'], algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect input",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": current_user.username}, expires_delta=access_token_expires
+
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
