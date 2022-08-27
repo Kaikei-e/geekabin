2d05c813b5477447cc59ff5d59e4fe411f7116fb
Kaikei-e <e_kaikei@protonmail.com>
2022-08-27 13:50:18 +0900

    Update: styling home page

--- frontend/apps/frontgeek/frontgeek/src/App.tsx
+++ frontend/apps/frontgeek/frontgeek/src/App.tsx
@@ -2,24 +2,24 @@ import type { Component } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
+import {Box} from "@suid/material/Box";
+import TextField from "@suid/material/TextField";
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <Box width={'100%'} minWidth={'100vw'}
+           height={'100%'} minHeight={'100vh'}
+           backgroundColor={'skyblue'}
+           padding={'5px'}
+           flexDirection={'column'}
+           alignItems={'flex-start'}
+      >
+          <Box width={'100%'} height={'100%'} textAlign={'start'}>
+              <text font-size={'20px'} color={'white'}>Geekabin</text>
+
+          </Box>
+      </Box>
     </div>
   );
 };
