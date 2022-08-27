4caed54f82283c15d2e37f7d6ceb68de8411bf23
Kaikei-e <e_kaikei@protonmail.com>
2022-08-27 23:16:05 +0900

    Update: Implementing User for auth

--- backend/apps/backgeek/authenticator/auth.py
+++ backend/apps/backgeek/authenticator/auth.py
@@ -1,7 +1,43 @@
+from typing import Union
+
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+import os
+from pydantic import BaseModel
+from passlib.context import CryptContext
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 
-async def oath_keeper(token: str = Depends(oauth2_scheme)):
-    return token
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    disabled: Union[bool, None] = None
+    uuid: str
+
+
+class UserInDB(User):
+    hashed_pass: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+print(os.environ['SECRET_KEY'])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def oath_keeper(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+
+
+
