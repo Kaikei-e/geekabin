8feb7a25b8003a52fe7fd0d8d4f24996a8768906
Kaikei-e <e_kaikei@protonmail.com>
2022-08-28 11:36:38 +0900

    Update: shakyoued JWT oauth FastAPI tutorial

--- backend/apps/backgeek/authenticator/auth.py
+++ backend/apps/backgeek/authenticator/auth.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta
 from typing import Union
 
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, APIRouter, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import os
 from pydantic import BaseModel
 from passlib.context import CryptContext
@@ -11,6 +11,8 @@ from jose import JWTError, jwt
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 SECRET_KEY = os.environ['SECRET_KEY']
+
+router = APIRouter()
 
 fake_users_db = {
     "johndoe": {
@@ -119,6 +121,21 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
         data={"sub": current_user.username}, expires_delta=access_token_expires
 
     )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/login/access-token", response_model=Token)
+async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = oath_keeper(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+
+        )
+    access_token_expire = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expire)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
